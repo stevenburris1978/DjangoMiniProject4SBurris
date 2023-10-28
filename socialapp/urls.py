4227fa1fb28725a20d08b545ee662566ecf20293
stevenburris1978 <s_burris@mail.fhsu.edu>
2023-10-28 09:59:24 -0500

    added urls and views code for index template

--- socialapp/urls.py
+++ socialapp/urls.py
@@ -3,6 +3,12 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
+    # ex: /socialapp/
     path("", views.index, name="index"),
+    # ex: /socialapp/5/
+    path("<int:question_id>/", views.detail, name="detail"),
+    # ex: /socialapp/5/results/
+    path("<int:question_id>/results/", views.results, name="results"),
+    # ex: /socialapp/5/vote/
+    path("<int:question_id>/vote/", views.vote, name="vote"),
 ]
-
