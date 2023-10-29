1fc97ea53648df5c2264a27f4e09327597be5efb
stevenburris1978 <s_burris@mail.fhsu.edu>
2023-10-28 20:07:36 -0500

    added results template

--- socialapp/urls.py
+++ socialapp/urls.py
@@ -3,12 +3,8 @@ from django.urls import path
 from . import views
 app_name = 'socialapp'
 urlpatterns = [
-    # ex: /socialapp/
-    path("", views.index, name="index"),
-    # ex: /socialapp/5/
-    path("<int:question_id>/", views.detail, name="detail"),
-    # ex: /socialapp/5/results/
-    path("<int:question_id>/results/", views.results, name="results"),
-    # ex: /socialapp/5/vote/
+    path("", views.IndexView.as_view(), name="index"),
+    path("<int:pk>/", views.DetailView.as_view(), name="detail"),
+    path("<int:pk>/results/", views.ResultsView.as_view(), name="results"),
     path("<int:question_id>/vote/", views.vote, name="vote"),
 ]
